75445a8a93d574627693f216d485e82d8ab16b02
Zach Grande <zach.grande23@gmail.com>
2023-05-16 08:35:04 -0700

    gh pages setup

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<p>*/}
+        {/*  Edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
+        <iframe
+          src="https://www.chatbase.co/chatbot-iframe/HxVmEy3Dl2QSFGo0P8scD"
+          width="100%"
+          height="700"
+          frameBorder="0"
+        ></iframe>
       </header>
     </div>
   );
